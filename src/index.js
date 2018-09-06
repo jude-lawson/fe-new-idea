468f12f5a21d070c4b31cbcdfbcbdd1ec625aa3f
Andrew James <ajames20@gmail.com>
2018-09-05 18:21:45 -0600

    Add redux and deps

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+import { Provider } from 'react-redux';
+import configureStore from './redux-store/configureStore';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/redux-store/configureStore.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+export default () => {
+  const store = createStore(
+    combineReducers({}),
+    composeWithDevTools(applyMiddleware(thunk))
+  );
+  return store;
+};
