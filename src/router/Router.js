38190249863777009f118257c446db9a2f176d59
Andrew James <ajames20@gmail.com>
2018-09-09 13:24:55 -0600

    Updage router nameing

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { Router, Route, Switch  } from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory';
-
-export const history = createHistory();
 
 export const AppRouter = () => (
-  <Router history={history}>
+  <Router >
     <Switch>
       <Route path="/" exact />
     </Switch>
