79d3b17832651d439cf9161ac7065393f167defb
Andrew James <ajames20@gmail.com>
2018-09-09 13:58:49 -0600

    Organize imports

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import AppRouter from './router/Router';
-
 import { Provider } from 'react-redux';
 import configureStore from './redux-store/configureStore';
 import registerServiceWorker from './registerServiceWorker';
+
+import AppRouter from './router/Router';
+
+import './index.css';
 
 const store = configureStore();
 
