efa38edc9d3686ad5b77b6151db4acaae694e2c1
Andrew James <ajames20@gmail.com>
2018-09-09 13:19:29 -0600

    Add history object

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Router, Route, Switch  } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+
+export const history = createHistory();
 
 export const AppRouter = () => (
-  <Router >
+  <Router history={history}>
     <Switch>
       <Route path="/" exact />
     </Switch>
