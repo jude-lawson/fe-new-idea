da8a6d8f524664cf6dc99eb0342d07c30503edde
Andrew James <ajames20@gmail.com>
2018-09-09 15:27:50 -0600

    Add test coverage for user auth flow action

---
+++ src/test/actions/auth.test.js
@@ -0,0 +1,23 @@
+import { userLoginAction } from '../../actions/auth';
+
+describe('userLoginAction', () => {
+  const id = 1;
+  const name = 'Steve';
+  const image = 'some-url';
+  const mockFirebaseResponse = {
+    uid: id,
+    displayName: name,
+    photoURL: image
+  };
+ test('should have a type of LOGIN', () => {
+  const expected = {
+    type: 'LOGIN',
+    user: {
+      ...mockFirebaseResponse
+    }
+  };
+
+  const result = userLoginAction(mockFirebaseResponse);
+  expect(result).toEqual(expected)
+ });
+});
