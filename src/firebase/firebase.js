658fa31c7d31ec23e5f4f00bd69c0fc2125be364
Andrew James <ajames20@gmail.com>
2018-09-09 13:56:38 -0600

    Add enviroment keys to project

---
+++ src/firebase/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase';
+import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN } from '../env.js';
+
+const config = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN
+};
+
+firebase.initializeApp(config);
+
+export const githubAuthProvider = new firebase.auth.GithubAuthProvider();
+export const githubOAuthLogin = () => firebase.auth().signInWithPopup(githubAuthProvider);
+
+
+export { firebase };
