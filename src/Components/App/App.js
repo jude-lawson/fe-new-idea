5509cb7aa3e6e03e5d61e1a5df2877c0c58e2f70
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-09-10 14:20:32 -0600

    Create handleChange fn to setState of ArticleForm for user input

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import {Header} from '../Header/Header';
+import { ArticleForm } from '../ArticleForm/ArticleForm';
 
 export class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <ArticleForm />
       </div>
     );
   }
