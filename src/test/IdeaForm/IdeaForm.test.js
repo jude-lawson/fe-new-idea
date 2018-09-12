a72642ba7b4482b8764e36c5c6db790e88c6ad75
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-09-12 08:55:39 -0600

    Pass IdeaForm MSTP test

--- src/test/IdeaForm/IdeaForm.test.js
+++ src/test/IdeaForm/IdeaForm.test.js
@@ -45,4 +45,60 @@ describe('IdeaForm', () => {
       expect(spy).toHaveBeenCalled();
     });
   });
+
+  describe('mapStateToProps', () => {
+    it('should create a props object with the correct keys', () => {
+      const mockState = {
+        ideas: [
+          {
+            title: 'Latest and Greatest',
+            body: 'Is it or is it not the latest and greatest?',
+            id: 11
+          }
+        ]
+      }
+      const expected = {
+        ideas: [
+        {
+          title: 'Latest and Greatest',
+          body: 'Is it or is it not the latest and greatest?',
+          id: 11
+        }
+        ]
+      };
+      const mappedProps = mapStateToProps(mockState);
+
+      expect(mappedProps).toEqual(expected);
+
+    });
+  }); 
+
+  // describe('mapDispatchToProps', () => {
+  //   it('should call dispatch when using toggleUserLogin from MDTP', () => {
+  //     const mockDispatch = jest.fn();
+  //     const actionToDispatch = toggleUserLogin({id: 3, loginStatus: true});
+  //     const mappedToProps = mapDispatchToProps(mockDispatch);
+  //     mappedToProps.toggleUserLogin({id: 3, loginStatus: true});
+      
+  //     expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+  //   });
+
+  //   it('should call dispatch when using userIsFalse from MDTP', () => {
+  //     const mockDispatch = jest.fn();
+  //     const actionToDispatch = userIsFalse({id: 3, loginStatus: true});
+  //     const mappedToProps = mapDispatchToProps(mockDispatch);
+  //     mappedToProps.userIsFalse({id: 3, loginStatus: true});
+      
+  //     expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+  //   });
+
+  //   it('should call dispatch when using addAllFavs from MDTP', () => {
+  //     const mockDispatch = jest.fn();
+  //     const actionToDispatch = addAllFavs([{id: 3, name: 'jaws'}]);
+  //     const mappedToProps = mapDispatchToProps(mockDispatch);
+  //     mappedToProps.addAllFavs([{id: 3, name: 'jaws'}]);
+      
+  //     expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+  //   });
+  // });
 });
