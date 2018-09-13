09abe64b36187e4379b8a397029bec72a51421cf
Andrew James <ajames20@gmail.com>
2018-09-13 17:58:16 -0600

    Add private routes for when use is authenticatd and tests

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Router, Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 import createHistory from 'history/createBrowserHistory';
+
 import App from '../Components/App/App';
+import { PrivateRoute } from './PrivateRoute';
 
 export const history = createHistory();
 
-export const AppRouter = () => (
+export const AppRouter = ({ authenticated }) => (
   <Router history={history}>
     <Switch>
       <Route path="/" exact component={App}/>
+      <PrivateRoute authenticated={authenticated} path="/profile" exact  component={App} />
     </Switch>
   </Router>
 );
 
-export default AppRouter;
+AppRouter.propTypes = {
+  authenticated: PropTypes.string
+};
+
+export const mapStateToProps = state => ({
+  authenticated: state.user.id
+});
+
+export default connect(mapStateToProps)(AppRouter);
