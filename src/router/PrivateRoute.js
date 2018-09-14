66b20f665cff98f6c2a840612a51d1f0673e9b69
Andrew James <ajames20@gmail.com>
2018-09-13 18:08:08 -0600

    Add default test for auth reducer

--- src/router/PrivateRoute.js
+++ src/router/PrivateRoute.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-export const PrivateRoute = ({ authenticated, component: Component, ...rest }) => console.log(rest)||(
+export const PrivateRoute = ({ authenticated, component: Component, ...rest }) => (
   <Route {...rest} render={props => (
     authenticated ?
       (
