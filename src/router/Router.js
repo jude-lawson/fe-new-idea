76418048ed73a7ca38371898e22aa6f9e44cf0b4
Andrew James <ajames20@gmail.com>
2018-09-17 07:00:31 -0600

    Add dynmic Links for an idea

--- src/router/Router.js
+++ src/router/Router.js
@@ -8,13 +8,20 @@ import App from '../Components/App/App';
 import Profile from '../Containers/Profile/Profile';
 import { PrivateRoute } from './PrivateRoute';
 import IdeaForm from '../Components/IdeaForm/IdeaForm';
+import { IdeaCard } from '../Containers/IdeaCard/IdeaCard';
 
 export const history = createHistory();
 
-export const AppRouter = ({ authenticated }) => (
+export const AppRouter = ({ authenticated, props }) => (
   <Router history={history}>
     <Switch>
       <Route path="/" exact component={App}/>
+      <Route path="/idea/:id" exact render={({ match }) => {
+        const idea = props.ideas.find(idea => idea.id === match.params.id);
+        return (
+          <IdeaCard {...idea}/>
+        );
+      }} />
       <PrivateRoute authenticated={authenticated} path="/ideaform" exact component={IdeaForm} />
       <PrivateRoute authenticated={authenticated} path="/profile" exact  component={Profile} />
     </Switch>
@@ -25,8 +32,9 @@ AppRouter.propTypes = {
   authenticated: PropTypes.string
 };
 
-export const mapStateToProps = state => ({
-  authenticated: state.user.id || true
+export const mapStateToProps = state => console.log(state) || ({
+  authenticated: state.user.id || true,
+  ideas: state.ideas || []
 });
 
 export default connect(mapStateToProps)(AppRouter);
