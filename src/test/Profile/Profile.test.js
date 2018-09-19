c3c5b65734baa36b7cf8114f933ce06e1ae12b83
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-09-19 10:38:28 -0600

    Test that ideas state is updated on componentDidMount

--- src/test/Profile/Profile.test.js
+++ src/test/Profile/Profile.test.js
@@ -9,8 +9,21 @@ describe('<Profile />', () => {
     id: '3214adf',
     image: 'some-url-here'
   }
+  const mockIdeas = [{title: 'hey', body: 'dude this is a body', id: 5}];
+  const mockMyIdeas = [{title: 'hey', body: 'second idea', id: 8}];
 
-  beforeEach(() => wrapper = shallow(<Profile user={mockUser}/>));
+
+  beforeEach(() => wrapper = shallow(<Profile user={mockUser} ideas={mockIdeas} myIdeas={mockMyIdeas} />));
+
+  test('componentDidMount', () => {
+    const allMockIdeas = [
+      {title: 'hey', body: 'dude this is a body', id: 5},
+      {title: 'hey', body: 'second idea', id: 8}]
+    wrapper.instance().componentDidMount();
+
+
+    expect(wrapper.state('ideas')).toEqual(allMockIdeas)
+  })
 
   describe('mapStateToProps', () => {
     test('should map user to props', () => {
